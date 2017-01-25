ee2649ec1592cacd9efb123f038c573142a09a86
Abhilash Malla <abhilash576@gmail.com>
2017-01-25 01:30:45 -0700

    initial commit

--- clock-app/app.js
+++ clock-app/app.js
@@ -1,6 +1,7 @@
 var module = angular.module("mymodule",[]);
 module.controller("ctrl", Main);
 function Main($scope) {
+    $scope.username = "";
     $scope.x= new Date();
     $scope.updateTime = function() {
              $scope.x= new Date();
